6f2f5402ba917e0d0dc3ab09fcaefab3c5cba16b
ekaji <ekajionah@gmail.com>
2021-09-22 21:52:25 +0100

    locomotive scroll

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects = () => {
     return(
         <div className="anton flex flex-col items-center">
-            <h2 className="w-4/5 flex flex-col uppercase text-4xl">projects</h2>
+            <h2 className="w-3/4 flex flex-col uppercase text-4xl">projects</h2>
         </div>
     )
 }
