b5e1baa44e507ea77b07376fdf85f3688e66aa7a
ekaji <ekajionah@gmail.com>
2021-11-16 15:44:12 +0100

    added locomotive scroll

--- gatsby-node.js
+++ gatsby-node.js
@@ -47,5 +47,19 @@ exports.createPages = async ({ graphql, actions }) => {
     })
     })
   })
+}
 
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /locomotive-scroll/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
 }
