7d6fdcb3ba5e995087f3b76891cf74bd9f70ca91
ekaji <ekajionah@gmail.com>
2021-11-18 17:09:03 +0100

    fixed menu not closing on sellection on mobile

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -29,9 +29,9 @@ const Menu = () => {
                     //mobile
                     <div className={`bg-clip-padding backdrop-filter backdrop-blur-xl absolute right-0 w-screen ${menuState ? 'transform transition-all ease-in duration-500 -translate-y-110' : 'transform transition-all ease-in duration-1000 translate-y-0' } md:translate-y-110`} >
                         <div className='tracking-widest mx-auto pt-10 h-screen uppercase flex flex-col space-y-12 md:space-x-24 '>
-                            <button ><a className='tracking-widest'  href="#home" data-scroll-to >home</a> </button>
-                            <button ><a className='tracking-widest' href="#about" data-scroll-to >about</a> </button>
-                            <button ><a className='tracking-widest'  href="#projects" data-scroll-to >projects</a> </button>
+                            <button ><a className='tracking-widest'  href="#home" data-scroll-to onClick={handleClick}>home</a> </button>
+                            <button ><a className='tracking-widest' href="#about" data-scroll-to onClick={handleClick} >about</a> </button>
+                            <button ><a className='tracking-widest'  href="#projects" data-scroll-to onClick={handleClick} >projects</a> </button>
                             <span className='mx-auto space-y-4 md:space-x-12'>
                                 <button className='tracking-widest block border-gray-900 border rounded-full w-40 py-2 hover:bg-gray-900 hover:text-white'><a href="/blog" data-scroll-to >blog</a> </button>
                                 <button className='tracking-widest block bg-gray-900 rounded-full w-40 py-2 text-white hover:bg-white hover:text-gray-900 border hover:border-gray-900'><a href="#">contact</a> </button>
