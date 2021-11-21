11903f15078570318b571acbbf9fd7e46603a696
ekaji <ekajionah@gmail.com>
2021-11-21 08:24:15 +0100

    added border to about section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const About = () => {
     return(
-        <div id="about" className='h-screen md:h-4/5 md:mb-44 flex anton w-3/4 mx-auto space-x-2 ' data-scroll-section >
+        <div id="about" className='h-80vh my-4 md:h-screen md:h-4/5 md:mb-44 border rounded-full border-trueGray-900 flex anton w-4/5 mx-auto space-x-2 ' data-scroll-section >
             <h3 className='text-7xl my-auto md:text-9xl text-trueGray-500 tracking-tight' >who am i</h3>
             <p className='text-2xl my-auto md:text-3xl tracking-wider uppercase md:w-2/4' data-scroll data-scroll-speed='5'>
                 i am a frontend focused web/software developer, i love to solve real life problems using clean and scalable solutions.
