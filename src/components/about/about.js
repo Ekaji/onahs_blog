aa1b9f8b262b9188fe4c1badc7a1852841054a64
ekaji <ekajionah@gmail.com>
2021-11-21 21:48:28 +0100

    added stack section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,12 +1,30 @@
 import React from "react"
 
 const About = () => {
+
+    const stacks = ['html', 'css', 'javascript', 'react', 'react native', 'nodejs', 'mongoDb', 'express', 'jquery']
+
     return(
-        <div id="about" className='h-80vh my-4 md:h-screen md:h-4/5 md:mb-44 border rounded-full border-trueGray-900 flex anton w-4/5 mx-auto space-x-2 ' data-scroll-section >
-            <h3 className='text-7xl my-auto md:text-9xl text-trueGray-500 tracking-tight' >who am i</h3>
-            <p className='text-2xl my-auto md:text-3xl tracking-wider uppercase md:w-2/4' data-scroll data-scroll-speed='5'>
-                i am a frontend focused web/software developer, i love to solve real life problems using clean and scalable solutions.
-            </p>
+        <div>
+            <div id="about" className='
+             mt-14 md:h-70vh md:mb-44 w-4/5 mx-auto md:flex justify-end mb-4' data-scroll-section >
+                <div className='md:w-2/4'>
+                    <h3 className='text-4xl md:text-9xl text-trueGray-900 ml-2 tracking-tight anton' >who am i</h3>
+                    <p className='max-mobile md:min-w-full text-1xl md:text-3xl .tracking-wider ml-2 .uppercase  .anton .p-4 pt-4 text-trueGray-700' 
+                    // data-scroll data-scroll-speed='4'
+                    >
+                        i am a frontend focused web/software developer, i love to solve real life problems using clean and scalable solutions.
+                    </p>
+                </div>
+            </div>
+            <div id='stack' className=' w-full' data-scroll-section>
+                <div className='w-4/5 mx-auto'>
+                    <h3 className='text-4xl anton uppercase'>my stack</h3>
+                    <div className='pt-4'>
+                        {stacks.map((stack, i) => ( <div key={i} className='border rounded-3xl border-trueGray-700 px-6 py-1 text-base md:text-lg anton tracking-wide my-1 mr-1 inline-block'>{stack}</div>) )}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -8,55 +8,54 @@ const Menu = () => {
 
     return(
         <>
-        <div 
-        className={`blur-comp fixed right-0 w-screen anton z-20 
-                        ${menuState ? ' transform transition-all ease-in duration-900 ' 
-                        : 'transform transition-all ease-in duration-200 ' } 
-                         `
-                        } >
-            <div className="flex justify-center items-center flex-col my-5 z-20" >
-                <div className="w-3/4 flex flex-col items-end md:flex-row md:justify-end">
+            <div 
+            className={`blur-comp fixed right-0 w-screen anton z-20 
+                            ${menuState ? ' transform transition-all ease-in duration-900 ' 
+                            : 'transform transition-all ease-in duration-200 ' } 
+                            `
+                            } >
+                <div className="flex justify-center items-center flex-col my-5 z-20" >
+                    <div className="w-3/4 flex flex-col items-end md:flex-row md:justify-end">
                         
-                {menuOrNavbar === 'menu' ? 
-                    <>
-                        <div className="z-20 w-9 h-7 pointer overflow-hidden " >
-                            <ul className="anton" >
-                                <li className={`transform transition-all ease-in duration-300 ${ menuState ? '-translate-y-7' : ''}`} onClick={handleClick}>
-                                    <div className='text-stroke uppercase h-2/4 border-b-2 border-gray-900'> close </div>
-                                </li>
-                                <li className={`transform transition-all ease-in duration-300 ${ menuState ? '-translate-y-7' : ''}`} onClick={handleClick}>
-                                    <div className='text-stroke uppercase h-2/4 border-b-2 border-gray-900'> menu </div>
-                                </li>
-                            </ul>
-                        </div>
-                        
-                        <div className={`bg-clip-padding backdrop-filter backdrop-blur-xl absolute right-0 w-screen ${menuState ? 'transform transition-all ease-in duration-500 -translate-y-110' : 'transform transition-all ease-in duration-1000 translate-y-0' } `} >
-                            <div className='tracking-widest mx-auto pt-10 h-screen uppercase flex flex-col space-y-12 md:space-x-24 '>
-                                <button ><a className='tracking-widest'  href="#home" data-scroll-to onClick={handleClick}>home</a> </button>
-                                <button ><a className='tracking-widest' href="#about" data-scroll-to onClick={handleClick} >about</a> </button>
-                                <button ><a className='tracking-widest'  href="#projects" data-scroll-to onClick={handleClick} >projects</a> </button>
-                                <span className='mx-auto space-y-4 md:space-x-12'>
-                                    <button className='tracking-widest block border-gray-900 border rounded-full w-40 py-2 hover:bg-gray-900 hover:text-white'><a href="/blog" data-scroll-to >blog</a> </button>
-                                    <button className='tracking-widest block bg-gray-900 rounded-full w-40 py-2 text-white hover:bg-white hover:text-gray-900 border hover:border-gray-900'><a href="#">contact</a> </button>
-                                </span>
+                    {menuOrNavbar === 'menu' ? 
+                        <>
+                            <div className="z-20 w-9 h-7 pointer overflow-hidden " >
+                                <ul className="anton" >
+                                    <li className={`transform transition-all ease-in duration-300 ${ menuState ? '-translate-y-7' : ''}`} onClick={handleClick}>
+                                        <div className='text-stroke uppercase h-2/4 border-b-2 border-gray-900'> close </div>
+                                    </li>
+                                    <li className={`transform transition-all ease-in duration-300 ${ menuState ? '-translate-y-7' : ''}`} onClick={handleClick}>
+                                        <div className='text-stroke uppercase h-2/4 border-b-2 border-gray-900'> menu </div>
+                                    </li>
+                                </ul>
                             </div>
-                        </div>
-                    </>
-                    : //desktop
-                    <div className='tracking-widest md:flex md:space-x-24 md:uppercase'>
-                        <button> <a className='tracking-widest' href="#home" data-scroll-to >home</a> </button>
-                        <button> <a className='tracking-widest' href="#about" data-scroll-to >about</a> </button>
-                        <button><a className='tracking-widest' href="#projects" data-scroll-to >projects</a> </button>
-                        <span className='space-x-12'>
-                            <button className='tracking-widest border-gray-900 border rounded-full w-24 py-2 hover:bg-gray-900 hover:text-white'><a href="/blog" data-scroll-to >blog</a> </button>
-                            <button className='tracking-widest bg-gray-900 rounded-full w-24 py-2 text-white hover:bg-white hover:text-gray-900 border hover:border-gray-900'><a href="#">contact</a> </button>
-                        </span>
+                                
+                            <div className={`bg-clip-padding backdrop-filter backdrop-blur-xl absolute right-0 w-screen ${menuState ? 'transform transition-all ease-in duration-500 -translate-y-110' : 'transform transition-all ease-in duration-1000 translate-y-0' } `} >
+                                <div className='tracking-widest mx-auto pt-10 h-screen uppercase flex flex-col space-y-12 md:space-x-24 '>
+                                    <button ><a className='tracking-widest'  href="#home" data-scroll-to onClick={handleClick}>home</a> </button>
+                                    <button ><a className='tracking-widest' href="#about" data-scroll-to onClick={handleClick} >about</a> </button>
+                                    <button ><a className='tracking-widest'  href="#projects" data-scroll-to onClick={handleClick} >projects</a> </button>
+                                    <span className='mx-auto space-y-4 md:space-x-12'>
+                                        <button className='tracking-widest block border-gray-900 border rounded-full w-40 py-2 hover:bg-gray-900 hover:text-white'><a href="/blog" >blog</a> </button>
+                                        <button className='tracking-widest block bg-gray-900 rounded-full w-40 py-2 text-white hover:bg-white hover:text-gray-900 border hover:border-gray-900'><a href="#">contact</a> </button>
+                                    </span>
+                                </div>
+                            </div>
+                        </>
+                            : //desktop
+                        <div className='tracking-widest md:flex md:space-x-24 md:uppercase'>
+                            <button> <a className='tracking-widest' href="#home" data-scroll-to >home</a> </button>
+                            <button> <a className='tracking-widest' href="#about" data-scroll-to >about</a> </button>
+                            <button><a className='tracking-widest' href="#projects" data-scroll-to >projects</a> </button>
+                            <span className='space-x-12'>
+                                <button className='tracking-widest border-gray-900 border rounded-full w-24 py-2 hover:bg-gray-900 hover:text-white'><a href="/blog"  >blog</a> </button>
+                                <button className='tracking-widest bg-gray-900 rounded-full w-24 py-2 text-white hover:bg-white hover:text-gray-900 border hover:border-gray-900'><a href="#">contact</a> </button>
+                            </span>
+                        </div> 
+                    }            
                     </div> 
-                }            
-                </div>
-                
-            </div> 
-        </div>
+                </div> 
+            </div>
         </>
     )
 }
