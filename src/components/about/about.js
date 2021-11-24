cb89f7afb26fb3f2eac68152a44e8fd81937e8a6
ekaji <ekajionah@gmail.com>
2021-11-24 07:21:01 +0100

    style changes

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,15 +2,15 @@ import React from "react"
 
 const About = () => {
 
-    const stacks = ['html', 'css', 'javascript', 'react', 'react native', 'nodejs', 'mongoDb', 'express', 'jquery']
+    const stacks = ['html', 'css', 'javascript', 'nodejs', 'react', 'react native', 'mongoDb', 'express', 'jquery']
 
     return(
         <div>
             <div id="about" className='
-             mt-14 md:h-70vh md:mb-44 w-4/5 mx-auto md:flex justify-end mb-4' data-scroll-section >
+             mt-14 md:h-70vh md:mb-44 w-4/5 mx-auto md:flex justify-end mb-12' data-scroll-section >
                 <div className='md:w-2/4'>
-                    <h3 className='text-4xl md:text-9xl text-trueGray-900 ml-2 tracking-tight anton' >who am i</h3>
-                    <p className='max-mobile md:min-w-full text-1xl md:text-3xl .tracking-wider ml-2 .uppercase  .anton .p-4 pt-4 text-trueGray-700' 
+                    <h3 className='text-2xl md:text-5xl  ml-2 tracking-tight anton uppercase text-trueGray-700' >who am i</h3>
+                    <p className='max-mobile md:min-w-full text-1xl md:text-3xl ml-2 tracking-wide anton pt-4 ' 
                     // data-scroll data-scroll-speed='4'
                     >
                         i am a frontend focused web/software developer, i love to solve real life problems using clean and scalable solutions.
@@ -19,9 +19,15 @@ const About = () => {
             </div>
             <div id='stack' className=' w-full' data-scroll-section>
                 <div className='w-4/5 mx-auto'>
-                    <h3 className='text-4xl anton uppercase'>my stack</h3>
+                    <h3 className='text-2xl anton uppercase text-trueGray-700'>my stack</h3>
                     <div className='pt-4'>
-                        {stacks.map((stack, i) => ( <div key={i} className='border rounded-3xl border-trueGray-700 px-6 py-1 text-base md:text-lg anton tracking-wide my-1 mr-1 inline-block'>{stack}</div>) )}
+                        {stacks.map((stack, i) => (
+                        <div key={i} 
+                             className='border rounded-3xl border-trueGray-700 px-6 py-1 text-base md:text-lg 
+                                        anton tracking-wide my-1 mr-2 inline-block '>
+                            {stack}
+                        </div>) 
+                        )}
                     </div>
                 </div>
             </div>
--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -2,31 +2,35 @@ import React from 'react'
 
 const Work = () => {
     return(
-        <div id='work' className='w-4/5 mx-auto mt-14 min-h-80vh border-b border-t border-gray-700 ' data-scroll-section>
-            <div className='md:w-3/4 h-80vh mx-auto flex flex-col md:flex-row md:border-l border-gray-700' >
-                {/* <div className='md:w-2/4 md:border-r mb-12 border-gray-700'>
-                    <h3 className=' md:min-w-min text-9xl anton'>selected works</h3>
-                </div> */}
-                <div className='min-w-1/4 md:writing-mode-vertical cursor-pointer'>
-                    <div className='capitalize border-b md:border-b-0 md:border-r border-gray-700  md:h-full text-2xl md:text-3xl'>
-                       <p className='py-2 md:px-4 anton'> landerdot </p>
-                    </div>
-                    <div className='capitalize border-b md:border-b-0 md:border-r border-gray-700  md:h-full text-2xl md:text-3xl'>
-                        <p className='py-2 md:px-4 anton'>anylease</p>
-                    </div>
-                    <div className='capitalize border-b md:border-b-0 md:border-r border-gray-700  md:h-full text-2xl md:text-3xl'>
-                        <p className='py-2 md:px-4 anton'>employee management portal</p>
-                    </div>
-                    <div className='capitalize border-b md:border-b-0 md:border-r border-gray-700  md:h-full text-2xl md:text-3xl'>
-                        <p className='py-2 md:px-4 anton'>dragnote</p>
-                    </div>
-                    <div className='capitalize border-b md:border-b-0 md:border-r border-gray-700 md:h-full text-2xl md:text-3xl'>
-                        <p className='py-2 md:px-4 anton'>bookmark landing</p>
+        <div>
+            <div id='work-section-header'>
+                <div>work work work work work work work work</div>
+                <div></div>
+            </div>
+            <div id='work' className='w-4/5 mx-auto mt-14 md:min-h-80vh border-t border-b-0 md:border-b border-gray-700 ' data-scroll-section>
+                <div className='md:w-3/4 md:h-80vh mx-auto flex flex-col md:flex-row md:border-l border-gray-700 ' >
+                    
+                    <div className='min-w-1/4 md:writing-mode-vertical cursor-pointer'>
+                        <div className='capitalize border-b md:border-b-0 md:border-r border-gray-700  md:h-full text-2xl md:text-3xl'>
+                           <p className='py-2 md:px-4 anton'> landerdot </p>
+                        </div>
+                        <div className='capitalize border-b md:border-b-0 md:border-r border-gray-700  md:h-full text-2xl md:text-3xl'>
+                            <p className='py-2 md:px-4 anton'>anylease</p>
+                        </div>
+                        <div className='capitalize border-b md:border-b-0 md:border-r border-gray-700  md:h-full text-2xl md:text-3xl'>
+                            <p className='py-2 md:px-4 anton'>employee management portal</p>
+                        </div>
+                        <div className='capitalize border-b md:border-b-0 md:border-r border-gray-700  md:h-full text-2xl md:text-3xl'>
+                            <p className='py-2 md:px-4 anton'>dragnote</p>
+                        </div>
+                        <div className='capitalize border-b md:border-b-0 md:border-r border-gray-700 md:h-full text-2xl md:text-3xl'>
+                            <p className='py-2 md:px-4 anton'>bookmark landing</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+        )
 }
 
 export default Work
