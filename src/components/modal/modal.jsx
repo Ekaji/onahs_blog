922276737ba04fd742279e23825ca59fa9d3a1de
ekaji <ekajionah@gmail.com>
2021-12-05 16:30:23 +0100

    update: images and link in modal, Fixed behaviour of modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -15,21 +15,19 @@ import React, { useState} from 'react'
                 </svg>
           </button>
         </div>
-        <div className="w-4/5  m-auto z-50 ">  
-            <div className="-white shadow-md border border-gray-200 rounded-lg m-auto max-w-sm md:max-w-screen-sm md:max-h-screen-sm md:flex ">
-                <a href="#">
-                    <img className="rounded-t-lg" src={modalDetails.image} alt="" />
+        <div className="w-10/12 m-auto z-50 p-4">  
+            <div className="-white shadow-md border border-gray-200 rounded-lg m-auto max-w-sm md:min-w-full md:flex ">
+            <img className="rounded-t-lg md:w-4/6" src={modalDetails.image} alt="" />
+            <div className="p-5 ">
+             <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">{modalDetails.name}</h5>
+             <p className="h-4/6 font-normal text-gray-700 mb-3 dark:text-gray-400">{modalDetails.description}</p>
+             <a href={modalDetails.link}
+                target='blank' 
+                className="justify-self-end text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    more
+                    <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </a>
-                <div className="p-5">
-                    <a href="#">
-                        <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">{modalDetails.name}</h5>
-                    </a>
-                    <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">{modalDetails.description}</p>
-                    <a href="#" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        more
-                        <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                    </a>
-                </div>
+            </div>
             </div> 
         </div>
       </div>
--- src/data/data.js
+++ src/data/data.js
@@ -1,26 +1,32 @@
 import anyleaseDesktop from '../../src/images/desktop/anyleaseDesktop.png'
 import landadotDesktop from '../../src/images/desktop/landerdotDesktop.png'
+import bookmarkDesktop from '../../src/images/desktop/bookmarkDesktop.png'
+import empDesktop from '../../src/images/desktop/empDesktop.png'
+
 
 export const projects = [
     {
         name: 'landadot',
         description: 'photography website for photographer isaac also known as landerdot',
-        link: '',
+        link: 'https://landadotphotography.vercel.app/',
         image: landadotDesktop,
+        repo: '',
         stack: ['react', 'tailwindcss']
     },
     {
         name: 'anylease',
         description: 'anylease is a website for leasing out equipments, the project was developed in collaboration with other developers during the zuri internship',
-        link: '',
+        link: 'https://anylease35.netlify.app/',
         image: anyleaseDesktop,
+        repo: '',
         stack: ['html', 'css', 'javascript']
     },
     {
         name: 'employee management portal',
         description: '',
-        link: '',
-        image: '',
+        link: 'https://eka-management-app.herokuapp.com/',
+        image: empDesktop,
+        repo: '',
         stack: ''
     },
     {
@@ -28,12 +34,14 @@ export const projects = [
         description: 'simple app for taking notes built using react native',
         link: '',
         image: '',
+        repo: '',
         stack: ''
     }, {
         name: 'bookmark landing page',
         description: 'landing page for a bookmark manager, it is one of the challenges that can be found on frontend mentors',
-        link: '',
-        image: '',
+        link: 'https://front-end-mentor-challenges-delta.vercel.app/bookmark-landing-page/index.html',
+        image: bookmarkDesktop,
+        repo: '',
         stack: ['html', 'css', 'javascript']
     },
 ];
